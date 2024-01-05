937fe589d05b22ce184b3b215f1f70a106d43019
Rhiannon <rhiannoncglau@gmail.com>
2024-01-05 14:55:09 -0500

    created new files

---
+++ applications.js
@@ -0,0 +1,7 @@
+function open_sidebar() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function close_sidebar() {
+    document.getElementById("mySidebar").style.display = "none";
+}
