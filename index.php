0ef6e2c046aaaeeedf1e7b0a2cc2e491f68a79fc
rhiannonlau <rhiannoncglau@gmail.com>
2024-01-11 14:14:31 -0500

    finer sidebar details

--- index.php
+++ index.php
@@ -12,27 +12,30 @@
 </head>
 <body>
     <!-- Sidebar -->
-    <div id="mySidebar" class="sidebar" style="display:none">
-        <button onclick="close_sidebar(); show_sidebar_button()" class="sidebarItem sidebarClose large">Close &times;</button>
-        <a href='resumePage.php' class="sidebarItem sidebarButton">Resume</a>
+    <div id="mySidebar" class="sidebar sidebarFromLeft" style="display:none">
+        <button onclick="close_sidebar(); show_sidebar_button()" class="sidebarItem sidebarCloseBtn large">Close &times;</button>
+        <a href='resumePage.php' class="sidebarItem sidebarBtn">Resume</a>
         <div id="drop" class="sidebarDropdown">
             <button class="sidebarDropdownBtn" href="projectsPage.php">Projects <span>&#x25BC;</span></button>
             <div id="projectsDrop" class="sidebarDropdownContent">
-                <a href="#" class="sidebarItem sidebarButton">Project 1</a>
-                <a href="#" class="sidebarItem sidebarButton">Project 2</a>
+                <a href="#" class="sidebarItem sidebarBtn">Project 1</a>
+                <a href="#" class="sidebarItem sidebarBtn">Project 2</a>
             </div>
         </div>
-        <a href='aboutMePage.php' class="sidebarItem sidebarButton">About Me</a>
+        <a href='aboutMePage.php' class="sidebarItem sidebarBtn">About Me</a>
     </div>
 
     <!-- Page -->
     <div>
-        <button id="sidebarOpen" onclick="open_sidebar()" class="sidebarOpenButton xlarge">☰</button>
+        <button id="sidebarOpen" onclick="open_sidebar()" class="sidebarOpenBtn xlarge"><span>&#9776;</span></button>
         <h1 class="white">Rhiannon Lau</h1>
     </div>
 
     <div>
-        <h2 class="white">Current projects</h2>
+        <h2 class="white">Current project</h2>
+        <div>
+
+        </div>
     </div>
 </body>
 </html>
