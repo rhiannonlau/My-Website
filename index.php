945191b54aa8d2089fa83e6772d35417dca4d648
rhiannonlau <rhiannoncglau@gmail.com>
2024-01-14 16:03:57 -0500

    finished about me page, added linkedin button

--- index.php
+++ index.php
@@ -8,12 +8,15 @@
 
     <!-- Link to CSS and JS: -->
     <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="sidebar.css">
+    <link rel="stylesheet" href="universal.css">
     <script src="applications.js"></script>
 </head>
 <body>
     <!-- Sidebar -->
     <div id="mySidebar" class="sidebar" style="display: none;">
         <button onclick="animate_sidebar()" class="sidebarItem sidebarCloseBtn large">Close &times;</button>
+        <a href='index.php' class="sidebarItem sidebarBtn">Home</a>
         <a href='resumePage.php' class="sidebarItem sidebarBtn">Resume</a>
         <div id="drop" class="sidebarDropdown">
             <button class="sidebarDropdownBtn" href="projectsPage.php">Projects <span>&#x25BC;</span></button>
@@ -28,7 +31,7 @@
     <!-- Page -->
     <div>
         <button id="sidebarOpenBtn" onclick="animate_sidebar()" class="sidebarOpenBtn xlarge"><span>&#9776;</span></button>
-        <a target="_blank" href="https://github.com/rhiannonlau" style="position: absolute; top: 8px; right: 16px">
+        <a target="_blank" href="https://github.com/rhiannonlau" style="position: absolute; top: 8px; right: 16px"> <!-- target="_blank" opens link in new tab -->
             <img src="res/github_logo_transparent.png" alt="Link to my Github"
                 style="
                 padding: 5px;
@@ -36,10 +39,16 @@
                 -webkit-filter: invert(100%);
                 filter: invert(100%) brightness(85%);">
         </a>
+        <a target="_blank" href="https://www.linkedin.com/in/rhiannon-lau-410362273/" style="padding-top: 15px; position: absolute; top: 50px; right: 16px"> <!-- next: place the two logos side by side-->
+            <img src="res/linkedin_logo_transparent.webp" alt="Link to my LinkedIn"
+                style="
+                width: 100px;
+                filter: brightness(85%);">
+        </a>
     </div>
 
-    
-    <h1 class="white">Current project</h1>
+    <h1 class="white">Home</h1>
+    <h2 class="white">Current project</h2>
     <div style="padding: 0 20% 0 20%">
         <div class="currProjCard">
             <img src="res/cat.png" alt="" style="width: 100%" class="center">
